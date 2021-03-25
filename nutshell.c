d21c3580f2ee807d5f3e457180f6590a4cd8e3a2
Kevin Karch <kevkar@ufl.edu>
2021-03-25 09:27:14 -0400

    first changes
    added linked list, added username to prompt, cleaned up lexer slightly

--- nutshell.c
+++ nutshell.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "ns_parser.tab.h"
 extern int yyparse();
 
-int main()
+struct Node {
+	char* data;
+	struct Node* next;
+};
+
+struct Node* makeNode(struct Node* node, char* chars) {
+	node = (struct Node*)malloc(sizeof(struct Node));
+	node->data = chars;
+}
+
+int main(int argc, char* argv[])
 {
 	while(1) 
 	{
-		printf(">> ");
+		char *userName=getenv("USER");
+		printf("%s>> ",userName);
 		yyparse();
 	}
 
