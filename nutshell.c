065d04daf9305b8758f5bda3e46e8c17448ca501
Kevin Karch <kevkar@ufl.edu>
2021-03-25 10:50:32 -0400

    Update nutshell.c
    added print environment variables, working on cd

--- nutshell.c
+++ nutshell.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
 #include "ns_parser.tab.h"
 extern int yyparse();
+extern char** environ;
 
 struct Node {
-	char* data;
+	const char* data;
 	struct Node* next;
 };
 
-struct Node* makeNode(struct Node* node, char* chars) {
+struct Node* makeNode(struct Node* node, const char* chars) {
 	node = (struct Node*)malloc(sizeof(struct Node));
 	node->data = chars;
+}
+
+void printEnvVariables()
+{
+for (char** envVar = environ; *envVar != 0; envVar++)
+    printf("%s\n", *envVar);    
+}
+
+void changeDirectory(const char* directory)
+{
+	chdir(directory);
+}
+
+void setDirectoryToHome()
+{
+	char* home = getenv("HOME");
+	if(home)
+		changeDirectory(home);
+	else 
+		printf("There is no home!");
 }
 
 int main(int argc, char* argv[])
@@ -20,6 +42,7 @@ int main(int argc, char* argv[])
 	{
 		char *userName=getenv("USER");
 		printf("%s>> ",userName);
+
 		yyparse();
 	}
 
