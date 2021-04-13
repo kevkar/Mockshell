d5f78010bf3036590afbbf1e41ce52f2eca22732
Kevin Karch <58485484+kevkar@users.noreply.github.com>
2021-04-13 10:35:59 -0400

    Update command.h

--- command.h
+++ command.h
@@ -2,68 +2,93 @@
 #define COMMAND_H
 
 #include <iostream>
-#include <vector>
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sstream>
-#include <unistd.h>
-#include <algorithm>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <map>
-#include "Alias.h"
 
-using namespace std;
+struct Command
+{
+	char* 	command_name;
+	char* 	args[10];
+	int 	num_args;
 
-struct Command {
-	char* command_name;
-	char* command_path;
-
-	std::vector<char*> args;
-	
-	char* input;
-	char* output;
-
-	Command(char* name)
+	Command()
 	{
-		command_name = name;
-		command_path = nullptr;
-		input = nullptr;
-		output = nullptr;
+		command_name = nullptr;
+		num_args = 1;
+
+		for(int i = 0; i < 10; ++i) {
+			args[i] = NULL;
+		}
+
+	}
+
+	~Command()
+	{
+		free(command_name);
+
+		for(int i = 0; i < num_args; ++i) {
+			free(args[i]);
+		}
 	}
 };
 
-void print_command(Command);
-void execute_command(Command);
-void execute_built_in(std::string name,std::vector<char*> args);
+struct Command_Table
+{
+	Command* command[10];
 
-//parsing
-const char* do_parse(const char* word);
-char* tilde_parse(char* word);
-char* dot_dot_parse(char* word);
-char* one_dot_parse(char* word);
-bool has_valid_env_variable(char* word);
-char* transform_env_variable(char* word);
+	int 	num_cmds;
 
+	char*	input;
+	char*	output;
+	char*	err_file;
 
-//built in commands
-void setEnvironmentVariable(const char* variable, const char* value);
-void printEnvVariables();
-void unsetEnvironmentVariable(const char* variable);
-void changeDirectory(const char* directory);
-void changeDirectoryToHome();
-void printAliases();
-void doAlias(std::string name, std::vector<char*> args);
-void setAlias(std::vector<char*> args);
-char* transform_alias(char* name);
-void unAlias(std::vector<char*> args);
-bool wouldMakeInfiniteLoop(char* name, int depth);
+	bool	append_output;
+	bool	wait_for_exec;
+	bool	err_stdout;
 
-bool is_available(std::string file);
-bool is_readable(std::string file);
-bool is_writable(std::string file);
-bool is_executable(std::string file);
+	Command_Table()
+	{
+		num_cmds = 0;
+		input = nullptr;
+		output = nullptr;
+		err_file = nullptr;
+		append_output = false;
+		wait_for_exec = true;
+		err_stdout = false;
+	}
+
+	void reset()
+	{
+		if (input != nullptr) 
+		{ 
+			free(input);
+			input = nullptr;
+		}
+		if (output != nullptr)
+		{ 
+			free(output);
+			output = nullptr;
+		}
+		if (err_file != nullptr)
+		{
+			free(err_file);
+			err_file = nullptr;
+		}
+
+		for(int i = 0; i < num_cmds; ++i) {
+			delete command[i];
+		}
+
+		num_cmds = 0;
+		append_output = false;
+		wait_for_exec = true;
+		err_stdout = false;
+	}
+};
+
+void print_command_table(Command_Table*);
+
+void process_command_table(Command_Table*);
+
+bool verify_command_and_args(Command*);
+bool is_available(std::string);
 
 #endif
