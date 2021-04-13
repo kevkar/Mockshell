ca82c7102d6a7a096f7928a700f6f464217edbef
Kevin Karch <58485484+kevkar@users.noreply.github.com>
2021-04-13 10:31:10 -0400

    Update global.h

--- global.h
+++ global.h
@@ -4,10 +4,13 @@
 #include "command.h"
 #include <vector>
 #include <string>
+#include <map>
 
 extern Command_Table* cmd_tbl;
 
 extern std::vector<std::string> built_in_cmds;
+extern std::map<std::string,std::string> variableMap;
+extern std::map<std::string,std::string> aliasTable;
 
 extern bool DEBUG;
 
